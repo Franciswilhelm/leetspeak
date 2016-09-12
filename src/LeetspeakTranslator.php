c539e945f2ef8ec082a14bfb8b0f36206bd18928
Francis Prus and J. Ryan Conklin <francis@>
2016-09-12 16:16:47 -0700

    finished leetspeak translator.

--- src/LeetspeakTranslator.php
+++ src/LeetspeakTranslator.php
@@ -1,33 +1,56 @@
+
 <?php
-
-
-    class LeetspeakTranslator
+    class Leetspeak
     {
-        function makeLeetspeak($input_leet)
+        private $word;
+        function __construct($new_word)
         {
-            $changed_letters = array("e", "o", "I", "s");
+            $this->word = $new_word;
+        }
+        function setWord($new_word)
+        {
+            $this->word = $new_word;
+        }
+        function getWord()
+        {
+            return $this->word;
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- ?>
+    function makeLeetspeak($input)
+    {
+        $wordArr = explode(" ", $input);
+        $modifiedArray = array();
+        foreach ($wordArr as $key=>$word)
+        {
+            $initialArray = str_split($word, 1);
+            // var_dump($initialArray);
+            foreach ($initialArray as $key=>$letter)
+            {
+                if ($letter == "e")
+                    {   $eIndex = array_search($letter, $initialArray);
+                        array_push($modifiedArray, "3");
+                    }
+                elseif ($letter == "o")
+                    {   $eIndex = array_search($letter, $initialArray);
+                        array_push($modifiedArray, "0");
+                    }
+                elseif ($letter == "I")
+                    {   $eIndex = array_search($letter, $initialArray);
+                        array_push($modifiedArray, "1");
+                    }
+                elseif (($letter == "s") && ($key != 0))
+                    {
+                        array_push($modifiedArray, "z");
+                    }
+                else {
+                    array_push($modifiedArray, $letter);
+                }
+            }
+            array_push($modifiedArray, " ");
+            // $newWord = implode($modifiedArray);
+        }
+        array_pop($modifiedArray);
+        $newSentence = implode($modifiedArray);
+        return $newSentence;
+    }
+?>
---
+++ tests/LeetspeakTranslatorTest.php
@@ -0,0 +1,51 @@
+<?php
+    require_once "src/Leetspeak.php";
+    class LeetspeakGenerator extends PHPUnit_Framework_TestCase
+    {
+        function test_Leetspeak_E()
+        {
+            //Arrange
+            $input = "face";
+            //Act
+            $result = makeLeetspeak($input);
+            //Assert
+            $this->assertEquals("fac3", $result);
+        }
+        function test_Leetspeak_O()
+        {
+            //Arrange
+            $input = "octo";
+            //Act
+            $result = makeLeetspeak($input);
+            //Assert
+            $this->assertEquals("0ct0", $result);
+        }
+        function test_Leetspeak_I()
+        {
+            //Arrange
+            $input = "Icelandic";
+            //Act
+            $result = makeLeetspeak($input);
+            //Assert
+            $this->assertEquals("1c3landic", $result);
+        }
+        function test_Leetspeak_S()
+        {
+            //Arrange
+            $input = "swimmers";
+            //Act
+            $result = makeLeetspeak($input);
+            //Assert
+            $this->assertEquals("swimm3rz", $result);
+        }
+        function test_Leetspeak_sentence()
+        {
+            //Arrange
+            $input = "The snake named super sam is from salem";
+            //Act
+            $result = makeLeetspeak($input);
+            //Assert
+            $this->assertEquals("Th3 snak3 nam3d sup3r sam iz fr0m sal3m", $result);
+        }
+    }
+?>
